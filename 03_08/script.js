5be13f1d69d0696021d5bee354462a451c9a45a2
Voichita Maria <sabo_voichita@yahoo.com>
2025-09-04 16:03:25 +0200

    next exercise Practice description

--- 03_08/script.js
+++ 03_08/script.js
@@ -1,5 +1,15 @@
 /**
  * Create a Backpack object.
+ * Practice
+ * Take objects from life
+ * 1.Give each object a identifiable name
+ * 2. Create propeties to describe the objects and
+ * set their values
+ * 3. Find an object that has another object inside of it to
+ * create a nested object
+ * 4. Test your objects in the browser console by accessing the
+ * entire object and its specific properties
+ 
  */
 
 const backpack = {
