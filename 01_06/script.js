96a8d3fc93585354248394d8c555f6be9715c09b
Voichita Maria <sabo_voichita@yahoo.com>
2025-09-04 13:05:17 +0200

    comments

--- 01_06/script.js
+++ 01_06/script.js
@@ -6,7 +6,11 @@
 
 /* Multi-line comment
 See! this line is also commented out! */
-
+/**
+ * fuction updateBackpack()
+ * Outputs HTML
+ * @param {string} update
+ */
 const updateBackpack = (update) => {
   let main = document.querySelector("main"); // main is an element
   main.innerHTML = markup(backpack);
