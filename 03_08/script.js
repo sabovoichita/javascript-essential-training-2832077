1f03e3e5a1f08935c7218a8e3b5a5b6ec26f64f5
Voichita Maria <sabo_voichita@yahoo.com>
2025-09-05 20:32:39 +0200

    Objects methods

--- 03_08/script.js
+++ 03_08/script.js
@@ -9,7 +9,34 @@
  * create a nested object
  * 4. Test your objects in the browser console by accessing the
  * entire object and its specific properties
- 
+
+-Objects can contain their own functions.
+-These funcitions typically perform actions on the properties of the object
+- When a function is inside an object is called a "method"
+- each method is added to the object as a property
+-How methods work?
+- A parameter is a piece of data that is passed inside the function
+-That parameter is passed inside the function to set the new value for the property
+-A function is a program that does something
+- We use parentheses to capture any paramters passed to the function
+(these are the values the function can use)
+- We wrap curly brackets around the statements of the function
+-To make a function run, we call it by stating its name and adding parenteses
+(function call)
+-if we want to pass values to the function, we put these values inside the parentheses 
+and they become the parameters of the function
+-backpack.lidopen
+//false
+backpack.toggleLid(true);
+//undefined
+backpacklidOpen
+//true
+console.log("Left before",backpack.strapLength.left);
+//Left before:26
+backpack.newStrapLength(10, 15);
+console.log("Left after",backpack.strapLength.left);
+//Left after:10
+
  */
 
 const backpack = {
