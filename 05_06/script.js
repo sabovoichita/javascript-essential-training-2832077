56b7a0d822cce9336f8ec1caac20440e9d3e0259
Voichita Maria <sabo_voichita@yahoo.com>
2025-09-12 14:42:37 +0200

    Element.attributes

--- 05_06/script.js
+++ 05_06/script.js
@@ -2,6 +2,39 @@
  * Traverse the DOM tree using querySelector() and querySelectorAll()
  * @link https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelector
  * @link https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelectorAll
+ * Element.attributes returns a live collecction of all the attribute nodes
+ * registered to a specific node...
+ * It is a NAMEDNODEMAP, not an Array, so it has no Array methods
+ * An attributes is a more complex piece of data
+ * It has a key and a value and we need to manipulate just the key
+ * or just the value or both at any time
+ * document.querySelector("img").attributes
+ * //NamedNodeMap {0: src, 1: alt, src: src, alt: alt, length: 2}
+ *Methods for attributes:
+
+ -*hasAttribute - to check it it has or not that attribute
+document.querySelector("img").hasAttribute("src")
+//true
+document.querySelector("img").hasAttribute("title")
+//false
+
+*getAttribute - to get the value of that attribute
+document.querySelector("img").getAttribute("src")
+//'../assets/images/everyday.svg'
+document.querySelector("img").getAttribute("alt")
+//''
+
+*setAttribute - to set the value of an existing attribute or creating a 
+new attribute- first name the attribute I want to set then , the whatever 
+value I want to attribute to that attribute
+document.querySelector("img").setAttribute("alt","A drawing of a Backpack")
+undefined
+document.querySelector("img").setAttribute("title","This shouldnt be here")
+undefined
+
+*removeAttribute- to remove an attribute
+document.querySelector("img").removeAttribute("title","This shouldnt be here")
+undefined
  */
 
 import Backpack from "./Backpack.js";
